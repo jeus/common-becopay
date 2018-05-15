4f1c5e0ac0bc9674e73cbe2a97f94feb0cf5086f
Administrator <alkhandani@gmail.com>
2018-05-15 17:30:47 +0430

    %20 #86 @20

--- src/main/java/com/b2mark/common/coin/Bitcoin.java
+++ src/main/java/com/b2mark/common/coin/Bitcoin.java
@@ -12,6 +12,7 @@ package com.b2mark.common.coin;
 /**
  * Coin is currency that work in system 
  *TODO: have to change description in this project
+ * TEst 
  */
 public final class Bitcoin implements Coin {
 
