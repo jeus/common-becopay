02589e6bdb3a36668d25d20af854426a454789a9
Administrator <alkhandani@gmail.com>
2018-05-15 10:48:07 +0430

    refs:#86 %10 @10h

--- src/main/java/com/b2mark/common/coin/Coin.java
+++ src/main/java/com/b2mark/common/coin/Coin.java
@@ -10,7 +10,7 @@
 /**
  * TODO: implementation name a JSON in blockchain.info or from a open source library.
  * TODO: remove this test jenkins
- * Test For Hashtag 
+ * Test For Hashtag Change this again for test
  */
 package com.b2mark.common.coin;
 
